5b21d81d9861215027a675ae4ba90b4de8e26c77
Estella Cheng <yc8234@nyu.edu>
2026-04-30 21:27:58 -0400

    Delete CS/Class directory

--- CS/Class/password.py
+++
@@ -1,9 +0,0 @@
-# Password Protection
-user_password = input("What is the password? ")
-password = 'secret'
-
-if user_password == password:
-    print("Welcome!")
-else:
-    print("Try again!")
-    
--- CS/Class/secret.py
+++
@@ -1,9 +0,0 @@
-# Password Protection
-user_password = input("What is the password? ")
-user_password_lowercase = str.lower(user_password)
-password = 'secret'
-
-if user_password_lowercase == password:
-    print("Welcome!")
-else:
-    print("Try again!")
